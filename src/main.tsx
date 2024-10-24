f12a79227b4df53528448d9a5aa068bb8c1905ab
arlansantos <arlan05sts@gmail.com>
2024-10-23 21:09:21 -0300

    add Suspense, Router, and StyleSheetManager

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { Suspense } from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import { StyleSheetManager } from "styled-components";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <Suspense fallback={<div>Loading...</div>}>
+    <BrowserRouter>
+      <StyleSheetManager shouldForwardProp={(prop) => prop !== "shake"}>
+        <App />
+      </StyleSheetManager>
+    </BrowserRouter>
+  </Suspense>
+);
